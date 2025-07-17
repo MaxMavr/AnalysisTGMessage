4ae12013a2c0c1963286531e6d6eb55885b14fb2
MaxMavr <maxmavr2005@gmail.com>
2025-07-17 16:29:42 +0300

    init config

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+from config import MESSAGES_DB_PATH, OUTPUT_NAME
 from analysis import parsing
 from html_layout.html_output import make_html
 from json import load
-
-MESSAGES_DB_PATH = r'L:\video photo music\Telegram\Миша\result.json'
-OUTPUT_NAME = 'Миша'
 
 with open(MESSAGES_DB_PATH, 'r', encoding='utf-8') as file:
     data = load(file)
